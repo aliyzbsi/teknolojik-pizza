edbe8dc3637749ccd7161409445ec4f88d38e4ef
aliyzbsi <aliyzbsi68@hotmail.com>
2024-10-10 15:48:33 +0300

    update

--- src/components/Iteration-2/HomePageContent/HomePage.jsx
+++ src/components/Iteration-2/HomePageContent/HomePage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { toast, ToastContainer } from "react-toastify";
+import "../../../../Assets/Iteration-1-assets/logo.svg";
 
 function HomePage({ pizzaData, getAllPizzas }) {
   const history = useHistory();
@@ -26,12 +27,13 @@ function HomePage({ pizzaData, getAllPizzas }) {
       <section
         className="bg-center bg-cover bg-no-repeat h-full"
         style={{
-          backgroundImage: "url('Assets/Iteration-1-assets/home-banner.png')",
+          backgroundImage:
+            "url('../../../../Assets/Iteration-1-assets/home-banner.png')",
         }}
       >
         <div className="flex flex-col items-center p-6 md:p-10 lg:p-20 ">
           <img
-            src="Assets/Iteration-1-assets/logo.svg"
+            src="../../../../Assets/Iteration-1-assets/logo.svg"
             alt="home-page-logo"
             className="w-52 md:w-48 lg:w-64"
           />
